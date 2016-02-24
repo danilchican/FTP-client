96a9fc97a96270df0aa5ff04a7f0f0d07eb54649
Vladislav Danilchik <danilchican@mail.ru>
2016-02-24 11:01:24 +0300

    Created CommandLineClass for processing command line from user

--- Command.cpp
+++ Command.cpp
@@ -14,5 +14,30 @@ void Command::sendCommand(SOCKET sock, const char *command)
 	strcpy_s(buf, length + 1, command);
 	strcat_s(buf, 7, "\r\n");
 
-	send(sock, buf, strlen(buf), 0);
+	try
+	{
+		int backed_code = send(sock, buf, strlen(buf), 0);
+		if (backed_code <= 0)
+			throw backed_code;
+	}
+	catch (int code)
+	{
+		switch (code)
+		{
+		case 0:
+			cout << "Cannot send command. Connection closed..." << endl;
+			break;
+		case -1:
+			cout << "Cannot send command. Connection failed..." << endl;
+			break;
+		default:
+			cout << "Something error: " << WSAGetLastError() << endl;
+			break;
+		}
+	}
+	catch (...)
+	{
+		cout << "Something error: " << WSAGetLastError() << endl;
+	}
+	
 }
---
+++ CommandLine.cpp
@@ -0,0 +1,7 @@
+#pragma comment(lib, "wsock32.lib")
+
+#include <iostream>
+#include <Winsock.h>
+#include "headers\CommandLine.h"
+
+using namespace std;
